4794c7a3d83b39efd6c1727e23c8ab13a8ea64ca
Hinesh Kumar <hineshkumar46@gmail.com>
2023-02-04 23:28:59 +0500

    Complete MAD Project

---
+++ app/src/main/java/com/example/testing/ui/search/SearchViewModelFactory.kt
@@ -0,0 +1,10 @@
+package com.example.testing.ui.search
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.testing.ui.dashboard.DashboardViewModel
+
+class SearchViewModelFactory() : ViewModelProvider.Factory {
+
+
+}
